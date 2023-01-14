19fb7dfa82c06a597ea2c30909b40780f2e10d0f
saeedmdd <saeed.madadi7@gmail.com>
2023-01-14 19:32:02 +0330

    init

---
+++ src/Turaj.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Saeed\TestComposer;
+
+class Turaj
+{
+    public function __construct()
+    {
+        echo "this is test in turaj way!";
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists("turaj")) {
+    function turaj()
+    {
+        return new \Saeed\TestComposer\Turaj();
+    }
+}
